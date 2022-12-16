f930f9756c65207009e5f90686ee2eeb2e9591f6
Mervin Bratic <salj@kwik.se>
2022-12-16 12:01:48 +0100

    changed color

--- src/data/about.js
+++ src/data/about.js
@@ -2,7 +2,7 @@ const softSkills = ["Kommunikativ", "Social", "Allmänbildad", "Driftig", "Nyfik
 const languages = ["Svenska", "Engelska", "Bosniska"]
 
 const synopsis = [
-  "En självlärd Frontend-utvecklare med praktisk erfarenhet av att bygga projekt med branschbeprövad teknologi. Min karriär har varit inom cirkulär ekonomi av hemelektronik, men passionen för systemutveckling har påtvingat ett karriärsbyte. Expertis inom konsument-hårdvara trots allt; mitt nya mål är ett ingångsjobb jag kan bidra till, och en arbetskultur i vilken jag kan utvecklas.",
+  "En självlärd Frontend-utvecklare med praktisk erfarenhet av att bygga projekt med branschbeprövad teknologi. Min karriär har varit inom cirkulär ekonomi av hemelektronik, men passionen för systemutveckling har påtvingat ett karriärsbyte. Expertis inom konsument-hårdvara trots allt; mitt nya mål är ett ingångsjobb inom webbutveckling jag kan bidra till, och en arbetskultur i vilken jag kan utvecklas.",
   "Jag har en absolut trespråkighet i svenska, bosniska och engelska, både i tal och skrift. Haft inom yrket längre utomeuropeiska sejourer och känner mig trygg i alla typer av dynamiska, sociala och kulturella miljöer. Har stor intern drivkraft men prioriterar gruppen framför jaget.",
   "Som egenföretagare inom e-handel mot slutkonsument såväl som mot större företagskunder har jag även utvecklat rutin för kundbevarande och kommunikation mot kund."
 ]
