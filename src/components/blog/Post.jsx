37d843b861644b4137bcd6566291f70a016e3130
Mervin Bratic <mervin@kwik.se>
2026-05-16 20:33:34 +0200

    fix hightlighter

--- src/components/blog/Post.jsx
+++ src/components/blog/Post.jsx
@@ -72,8 +72,8 @@ function Post() {
             components={{
               code({ node, inline, className, children, ...props }) {
                 const match = /language-(\w+)/.exec(className || '')
-                return !inline && match ? (
-                  <SyntaxHighlighter style={oneDark} language={match[1]} PreTag="div" {...props}>
+                return match ? (
+                  <SyntaxHighlighter style={oneDark} language={match[1]} PreTag="div">
                     {String(children).replace(/\n$/, '')}
                   </SyntaxHighlighter>
                 ) : (
