952e454f22b42f17032d549325f56807d934f00c
Mervin Bratic <mervin@kwik.se>
2026-07-27 21:21:12 +0200

    fix turbopack workspace root

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  turbopack: {
+    root: process.cwd(),
+  },
   images: {
     unoptimized: true,
   },
