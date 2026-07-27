57a05bfe334365747c54a20881d4655a8643b63d
Mervin Bratic <mervin@kwik.se>
2026-07-27 23:04:04 +0200

    use webpack build for OpenNext

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,13 +1,8 @@
-import path from 'node:path'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  outputFileTracingRoot: path.resolve(__dirname, '..'),
-  turbopack: {
-    root: path.resolve(__dirname, '..'),
-  },
   images: {
     unoptimized: true,
   },
